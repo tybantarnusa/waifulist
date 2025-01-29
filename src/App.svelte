d5b15e766e204904fe4126bc51a3ebfd8a2dc475
Thoyib Antarnusa <tybantarnusa@null.net>
2025-01-29 10:30:03 +0700

    better styling

--- src/App.svelte
+++ src/App.svelte
@@ -24,7 +24,9 @@
   let fuzzySearch: FuzzySearcher<Waifu> | null = null;
 
   onMount(async () => {
-    allWaifus = (await d3.csv("https://raw.githubusercontent.com/tybantarnusa/waifulist/refs/heads/main/public/waifus.csv")) as unknown as Waifu[];
+    allWaifus = (await d3.csv(
+      "https://raw.githubusercontent.com/tybantarnusa/waifulist/refs/heads/main/public/waifus.csv",
+    )) as unknown as Waifu[];
     allWaifus.sort(() => Math.random() - 0.5);
 
     fuzzySearch = createFuzzySearch(allWaifus, {
@@ -50,34 +52,49 @@
 </script>
 
 <main class="has-background-black-ter">
-  <Header />
-  <div class="container columns">
-    <div class="column">
-      <div class="field">
-        <div class="control">
-          <input
-            class="input"
-            type="text"
-            placeholder="Search by name or series..."
-            bind:value={search}
-          />
+  <div>
+    <Header />
+    <div class="container columns">
+      <div class="column">
+        <div class="field">
+          <div class="control">
+            <input
+              class="input"
+              type="text"
+              placeholder="Search by name or series..."
+              bind:value={search}
+            />
+          </div>
         </div>
-      </div>
 
-      <div class="fixed-grid has-3-cols-desktop has-1-cols-mobile">
-        <div class="grid">
-          {#each waifus as waifu}
-            <Card {...waifu} />
-          {/each}
+        <div class="fixed-grid has-3-cols-desktop has-1-cols-mobile">
+          <div class="grid">
+            {#each waifus as waifu}
+              <Card {...waifu} />
+            {/each}
+          </div>
         </div>
-      </div>
 
-      {#if waifus.length < filteredWaifus.length}
-        <button
-          class="button is-white is-outlined is-fullwidth"
-          onclick={() => (size += 9)}>Load more</button
-        >
-      {/if}
+        {#if waifus.length < filteredWaifus.length}
+          <button
+            class="button is-white is-outlined is-fullwidth"
+            onclick={() => (size += 9)}>Load more</button
+          >
+        {/if}
+      </div>
     </div>
   </div>
 </main>
+
+<style>
+  main {
+    background: url("./bg.jpg");
+    background-attachment: fixed;
+    background-size: cover;
+    background-position: center;
+  }
+
+  main>div {
+      backdrop-filter: blur(5px);
+  }
+</style>
--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,14 +1,26 @@
 <div class="box">
-  <button type="button" class="header-title" onclick={() => { window.location.reload() }}>tybantarnusa's Waifu List</button>
+      <button
+        type="button"
+        class="header-title"
+        onclick={() => {
+          window.location.reload();
+        }}>tybantarnusa's Waifu List</button
+      >
 </div>
 
 <style>
+  @import url("https://fonts.googleapis.com/css2?family=Chewy&display=swap");
+
   .box {
     border-radius: 0px;
+    margin: 0px;
   }
 
   .header-title {
-    color: white;
+    color: #dddddd;
     font-size: 1.5rem;
+    font-family: "Chewy", serif;
+    font-weight: 400;
+    font-style: normal;
   }
 </style>
